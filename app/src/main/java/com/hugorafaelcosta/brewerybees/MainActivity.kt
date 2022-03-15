d61ca54ac55db4e808b6450e72098dd0b650bfec
Hugo Costa <costarafaelhugo@gmail.com>
2022-03-15 14:40:02 -0300

    Criei a splash screen

--- app/src/main/java/com/hugorafaelcosta/brewerybees/MainActivity.kt
+++ app/src/main/java/com/hugorafaelcosta/brewerybees/MainActivity.kt
@@ -6,6 +6,11 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Thread.sleep(2000)
+
+        setTheme(R.style.Theme_BreweryBEES)
+
         setContentView(R.layout.activity_main)
     }
 }
